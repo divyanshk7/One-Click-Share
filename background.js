8684140b425122d74f60f6c9e930e73622ab8cf8
Divyansh Kogta <42926672+divyanshk7@users.noreply.github.com>
2020-03-29 15:10:16 +0530

    Add files via upload

---
+++ background.js
@@ -0,0 +1,39 @@
+
+function MyImageClick(info, tab) {
+	console.log("Clicked an image", info, tab);
+	chrome.windows.create({
+		"url": "https://facebook.com/sharer.php?u=" + info.srcUrl + "&display=popup",
+		"type": "popup"
+	})	
+}
+
+function MyQuoteClick(info, tab) {
+	console.log("Clicked an image", info, tab);
+	chrome.windows.create({
+		"url": "https://facebook.com/sharer.php?u=" + info.pageUrl + "&display=popup&quote=" + info.selectionText,
+		"type": "popup"
+	})	
+}
+
+
+chrome.contextMenus.create(
+{
+	"title" : "Share Image",
+	"contexts" : ["image"],
+	"onclick" : MyImageClick
+}
+)
+
+chrome.contextMenus.create(
+{
+	"title" : "Share Quote",
+	"contexts" : ["selection"],
+	"onclick" : MyQuoteClick
+}
+)
+
+
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse){
+	console.log("message", msg)
+sendResponse({"text": "Recieved the links"});
+})
